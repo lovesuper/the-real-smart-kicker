0fdd656d51f8aaea77a51557895d4a44719a9c85
anton <antononrails@gmail.com>
2015-07-12 22:01:51 +0300

    Рефакторинг

--- the-real-smart-kicker/the-real-smart-kicker.ino
+++ the-real-smart-kicker/the-real-smart-kicker.ino
@@ -2,24 +2,33 @@
 #include <SoftwareSerial.h>
 #include <JQ6500_Serial.h>
 
-// LEDS
+// LEDS [next version]
 
-#define RED_LED_DATA 11
-#define RED_LED_LATCH 10
-#define RED_LED_CLOCK 9
-
-#define BLUE_LED_DATA 8
-#define BLUE_LED_LATCH 7
-#define BLUE_LED_CLOCK 6
+//#define RED_LED_DATA 11
+//#define RED_LED_LATCH 10
+//#define RED_LED_CLOCK 9
+//
+//#define BLUE_LED_DATA 8
+//#define BLUE_LED_LATCH 7
+//#define BLUE_LED_CLOCK 6
 
 //
 
 #define LDR_BLUE_PIN A0
 #define LDR_RED_PIN A1
+
+// Mp3 value control
 #define POT_VALUE_CONTROL A2
 
-#define BLUE_GOAL_THRESHOLD 350
-#define RED_GOAL_THRESHOLD 350
+//Real
+#define BLUE_GOAL_THRESHOLD 600
+#define RED_GOAL_THRESHOLD 600
+
+////Test
+//#define BLUE_GOAL_THRESHOLD 620
+//#define RED_GOAL_THRESHOLD 620
+
+#define GOALS_REVENGE_DELAY 10000
 
 JQ6500_Serial mp3(12,13);
 
@@ -51,11 +60,11 @@ JQ6500_Serial mp3(12,13);
 
 #define GOAL_SOUNDS_START 33
 #define GOAL_SOUNDS_END 42
-#define COW_SOUND 43
-#define CARRY_ON 44
-#define FLO 45
-#define APL 46
-#define ZOID 47
+
+#define APPL_1 43
+#define APPL_2 44
+#define CHICKEN 47
+#define TROMBONE 48
 
 boolean blueGoaled = false; // синим забили
 boolean redGoaled = false; // красным забили
@@ -79,54 +88,59 @@ long thresholdOfHopelessness = firstThresholdOfHopelessness;
 
 void setup() {
   mp3.begin(9600);
+  Serial.begin(230400);
   mp3.reset();
-  mp3.setVolume(18);
+  mp3.setVolume(17);
   mp3.playFileByIndexNumber(GAME_STARTED);
 
-  pinMode(RED_LED_DATA, OUTPUT);
-  pinMode(RED_LED_LATCH, OUTPUT);
-  pinMode(RED_LED_CLOCK, OUTPUT);
+//[next version]
+//  pinMode(RED_LED_DATA, OUTPUT);
+//  pinMode(RED_LED_LATCH, OUTPUT);
+//  pinMode(RED_LED_CLOCK, OUTPUT);
+//  pinMode(BLUE_LED_DATA, OUTPUT);
+//  pinMode(BLUE_LED_LATCH, OUTPUT);
+//  pinMode(BLUE_LED_CLOCK, OUTPUT);
+//  showBlueNumber(0);
+//  showRedNumber(0);
 
-  pinMode(BLUE_LED_DATA, OUTPUT);
-  pinMode(BLUE_LED_LATCH, OUTPUT);
-  pinMode(BLUE_LED_CLOCK, OUTPUT);
-
-  showBlueNumber(0);
-  showRedNumber(0);
+  Serial.print("Debugging TRSK starts here:");
+  Serial.print("\n");
 }
 
-byte segments[10] = {
-  0b00001000, 0b01101011, 0b01000100, 0b01000001, 0b00100011,
-  0b00010001, 0b00010000, 0b01001011, 0b00000000, 0b00000001
-};
+//[next version]
+//byte segments[10] = {
+//  0b00001000, 0b01101011, 0b01000100, 0b01000001, 0b00100011,
+//  0b00010001, 0b00010000, 0b01001011, 0b00000000, 0b00000001
+//};
 
-void showRedNumber(int number){
-  digitalWrite(RED_LED_LATCH, LOW);
-  shiftOut(RED_LED_DATA, RED_LED_CLOCK, LSBFIRST, segments[number]);
-  digitalWrite(RED_LED_LATCH, HIGH);  
-}
+//[next version]
+//void showRedNumber(int number){
+//  digitalWrite(RED_LED_LATCH, LOW);
+//  shiftOut(RED_LED_DATA, RED_LED_CLOCK, LSBFIRST, segments[number]);
+//  digitalWrite(RED_LED_LATCH, HIGH);
+//}
+//
+//[next version]
+//void showBlueNumber(int number){
+//  digitalWrite(BLUE_LED_LATCH, LOW);
+//  shiftOut(BLUE_LED_DATA, BLUE_LED_CLOCK, LSBFIRST, segments[number]);
+//  digitalWrite(BLUE_LED_LATCH, HIGH);
+//}
 
-void showBlueNumber(int number){
-  digitalWrite(BLUE_LED_LATCH, LOW);
-  shiftOut(BLUE_LED_DATA, BLUE_LED_CLOCK, LSBFIRST, segments[number]);
-  digitalWrite(BLUE_LED_LATCH, HIGH);  
+void printDebugInfo(char* team, int goals, int ldrValue) {
+    char msg[40];
+    sprintf(msg, "%s team goaled %d with LDR volume %d\n", team, goals, ldrValue);
+    Serial.print(msg);
 }
 
 boolean isRedGoaled() {
     int ldrRedLightness = analogRead(LDR_RED_PIN);
-
     boolean result = ldrRedLightness < RED_GOAL_THRESHOLD;
     if(result) {
         printDebugInfo("Red", redGoals + 1, ldrRedLightness);
     }
 
     return result;
-}
-
-void printDebugInfo(char* team, int goals, int ldrValue) {
-    char msg[40];
-    sprintf(msg, "%s team goaled %d with LDR volume %d\n", team, goals, ldrValue);
-    Serial.print(msg);
 }
 
 boolean isBlueGoaled() {
@@ -141,109 +155,71 @@ boolean isBlueGoaled() {
 }
 
 boolean checkSpecial() {
-    int randNum = rand()%(GOAL_SOUNDS_END-GOAL_SOUNDS_START + 1) + GOAL_SOUNDS_START;
+   int randNum = rand()%(GOAL_SOUNDS_END-GOAL_SOUNDS_START + 1) + GOAL_SOUNDS_START;
     mp3.playFileByIndexNumber(randNum);
     delay(2000);
-    int goalsDelay = abs(redLastGoalMills - blueLastGoalMills);
-    if(goalsDelay < 10000 && redLastGoalMills != 0 && blueLastGoalMills !=0){
-        mp3.playFileByIndexNumber(RAPID_EXCHANGE);
-        return true; 
-     }
-    
-    if((blueGoals + redGoals) == 1) {
-        mp3.playFileByIndexNumber(FIRST_BLOOD);
-        delay(2000);
-        mp3.playFileByIndexNumber(APL);
-        return true;
-    } else if (redFastGoalsCount == 3 || blueFastGoalsCount == 3) {
-        mp3.playFileByIndexNumber(THREE_FAST_GOALS);
-        if(redFastGoalsCount == 3)
-            redFastGoalsCount = 0;
-        else
-            blueFastGoalsCount = 0;
 
-        return true;
-    } else if (blueGoaledInRow == 2 || redGoaledInRow == 2) {
-        mp3.playFileByIndexNumber(TWO_GOALS_IN_ROW);
-        delay(2000);
-        mp3.playFileByIndexNumber(APL);
-        return true;
-    } else if (blueGoaledInRow == 3 || redGoaledInRow == 3) {
-        mp3.playFileByIndexNumber(THREE_GOALS_IN_ROW);
-        return true;
-    } else if (blueGoaledInRow == 4 || redGoaledInRow == 4) {
-        mp3.playFileByIndexNumber(FOUR_GOALS_IN_ROW);
-        return true;
-    } else if (blueGoaledInRow == 5 || redGoaledInRow == 5) {
-        mp3.playFileByIndexNumber(FIVE_GOALS_IN_ROW);
-        return true;
-    } else if (blueGoaledInRow == 6 || redGoaledInRow == 6) {
-        mp3.playFileByIndexNumber(SIX_GOALS_IN_ROW);
-        delay(2000);
-        mp3.playFileByIndexNumber(CARRY_ON);
-        delay(2000);
-        return true;
-    } else if (blueGoaledInRow == 7 || redGoaledInRow == 7) {
-        mp3.playFileByIndexNumber(SEVEN_GOALS_IN_ROW);
-        return true;
-    } else if (blueGoaledInRow == 8 || redGoaledInRow == 8) {
-        mp3.playFileByIndexNumber(EIGHT_GOALS_IN_ROW);
-        return true;
-    } else if (blueGoaledInRow == 9 || redGoaledInRow == 9) {
-        mp3.playFileByIndexNumber(NINE_GOALS_IN_ROW);
-        return true;
+    // Проверка на быстрый обмен голами
+    int goalsDelay = abs(redLastGoalMills - blueLastGoalMills);
+    if(goalsDelay < GOALS_REVENGE_DELAY && redLastGoalMills != 0 && blueLastGoalMills !=0){
+       mp3.playFileByIndexNumber(RAPID_EXCHANGE);
+       return true;
     }
+
+    // Проверка на первый гол
+    int fastGoalsCount = redFastGoalsCount + blueFastGoalsCount;
+    if((blueGoals + redGoals) == 1) {
+       mp3.playFileByIndexNumber(FIRST_BLOOD);
+       delay(2000);
+       return true;
+    // Проверка на мячи подряд (12-21)
+    } else if (fastGoalsCount > 0) {
+       mp3.playFileByIndexNumber(10 + fastGoalsCount);
+       return true;
+    }
+
     return false;
 }
 
 void resetMatch(){
-    delay(200);
     if (blueGoals > redGoals){
-        mp3.playFileByIndexNumber(RED_TEAM_WIN);
-        delay(2000);
-        mp3.playFileByIndexNumber(ZOID);
-        delay(3000);
+       mp3.playFileByIndexNumber(RED_TEAM_WIN);
     } else{
-        mp3.playFileByIndexNumber(BLUE_TEAM_WIN);
-        delay(1000);
-        mp3.playFileByIndexNumber(ZOID);
-        delay(3000);
+       mp3.playFileByIndexNumber(BLUE_TEAM_WIN);
     }
-
-    if (blueGoaledInRow == 10 || redGoaledInRow == 10){
-        mp3.playFileByIndexNumber(FLAWLESS_VICTORY);
-        delay(2000);
-        mp3.playFileByIndexNumber(COW_SOUND);
+    delay(2000);
+    if (blueGoaledInRow + redGoaledInRow == 10){
+       mp3.playFileByIndexNumber(FLAWLESS_VICTORY);
+       delay(2000);
     }
     blueGoals = 0;
     redGoals = 0;
-
     blueGoaled = false;
     redGoaled = false;
-
     blueGoaledInRow = 0;
     redGoaledInRow = 0;
-
     redFastGoalsCount = 0;
     blueFastGoalsCount = 0;
-    showBlueNumber(0);
-    showRedNumber(0);
-    mp3.playFileByIndexNumber(GAME_STARTED);
+// [next version]
+//    showBlueNumber(0);
+//    showRedNumber(0);
+   mp3.playFileByIndexNumber(GAME_STARTED);
 }
 
 void loop() {
-    int soundValue = map(analogRead(POT_VALUE_CONTROL), 0, 1023, 0, 30);
-    mp3.setVolume(soundValue);
+//    int soundValue = map(analogRead(POT_VALUE_CONTROL), 0, 1023, 0, 30);
+//    mp3.setVolume(soundValue);
     if(isBlueGoaled()){
         blueGoaled = true;
         blueGoals++;
         blueGoaledInRow++;
         redGoaledInRow = 0;
         pointsOfHopelessness = 0;
-        showBlueNumber(blueGoals);
-        if((millis() - redLastGoalMills) < 6000){
+        // [next version]
+        // showBlueNumber(blueGoals);
+        if((millis() - redLastGoalMills) < 8000){
             redFastGoalsCount++;
-            Serial.print(">Red fast goal number ");
+            Serial.print(">>> Red fast goal! number ");
             Serial.print(redFastGoalsCount);
             Serial.print("\n");
         } else {
@@ -252,10 +228,9 @@ void loop() {
 
         if (!checkSpecial()) {
             mp3.playFileByIndexNumber(blueGoals);
+            delay(1000);
         }
         redLastGoalMills = millis();
-
-        delay(1000);
     }
 
     if(isRedGoaled()){
@@ -264,41 +239,40 @@ void loop() {
         redGoals++;
         redGoaledInRow++;
         blueGoaledInRow = 0;
-        showRedNumber(redGoals);  
-        if((millis() - blueLastGoalMills) < 6000){
-            blueFastGoalsCount++;
-            Serial.print(">Blue fast goal number ");
-            Serial.print(blueFastGoalsCount);
-            Serial.print("\n");
-        } else {
-            blueFastGoalsCount = 0;
-        }
-        blueLastGoalMills = millis();
+        // [next version]
+        // showRedNumber(redGoals);
+       if((millis() - blueLastGoalMills) < 8000){
+           blueFastGoalsCount++;
+           Serial.print(">>> Blue fast goal! number ");
+           Serial.print(blueFastGoalsCount);
+           Serial.print("\n");
+       } else {
+           blueFastGoalsCount = 0;
+       }
+       blueLastGoalMills = millis();
 
         if (!checkSpecial()) {
             mp3.playFileByIndexNumber(redGoals);
+            delay(1000);
         }
-
-        delay(1000);
     }
 
     if(blueGoals >= 10 || redGoals >= 10) {
-        resetMatch();
+       resetMatch();
     }
 
     if(!isRedGoaled() && !isBlueGoaled()){
-        pointsOfHopelessness++;
+       pointsOfHopelessness++;
     }
 
     if(pointsOfHopelessness >= thresholdOfHopelessness) {
-        mp3.playFileByIndexNumber(LONG_TIME_NO_GOALS);
-        if(thresholdOfHopelessness == secondThresholdOfHopelessness){
-            delay(1500);
-            mp3.playFileByIndexNumber(HOPELESS);
-        }
+       mp3.playFileByIndexNumber(LONG_TIME_NO_GOALS);
+       if(thresholdOfHopelessness == secondThresholdOfHopelessness){
+           delay(1500);
+           mp3.playFileByIndexNumber(HOPELESS);
+       }
 
-        pointsOfHopelessness = 0;
-        thresholdOfHopelessness = secondThresholdOfHopelessness;
+       pointsOfHopelessness = 0;
+       thresholdOfHopelessness = secondThresholdOfHopelessness;
     }
 }
-
